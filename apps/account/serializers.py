e74469d09d163de25d9ba265b824f7bf376ed148
yaoshaoqiang <smxblysq@qq.com>
2022-12-11 21:05:56 +0800

    rsync code

---
+++ apps/account/serializers.py
@@ -0,0 +1,84 @@
+from abc import ABC
+import datetime
+from rest_framework import serializers
+from utils.crypt import AesCrypt
+from devops_api.settings import SALT_KEY, SECRET_KEY
+from django.contrib import auth
+from apps.account.models import UserInfo, UserToken
+from django.utils import timezone
+import time
+import hashlib
+
+
+class SignInSerializer(serializers.Serializer, ABC):
+    username = serializers.CharField(
+        allow_blank=False,
+        allow_null=False,
+        error_messages={
+            "required": "缺少用户名字段."
+        }
+    )
+    password = serializers.CharField(
+        allow_null=False,
+        allow_blank=False,
+        error_messages={
+            "required": "缺少密码字段.",
+            "min_length": "密码太短，至少8个字符."
+        }
+    )
+    crypt = AesCrypt(model='ECB', iv='', encode_='utf-8', key=SALT_KEY)
+
+    def validate(self, attrs):
+        """
+        :param attrs:
+        :return:
+        """
+        attrs['password'] = self.crypt.aesdecrypt(attrs['password'])
+        if not attrs['password']:
+            raise serializers.ValidationError('解码密码异常，请检查！')
+        user_obj = auth.authenticate(**attrs)
+        if not user_obj:
+            raise serializers.ValidationError(detail="登录失败，用户名或者密码错误！{}".format(attrs), code="auth")
+        UserInfo.objects.filter(**attrs).update(last_login=datetime.datetime.now())
+        return attrs
+
+    def validated_username(self, attrs):
+        for key, value in attrs.items():
+            attrs[key] = self.crypt.decrypt_text(value)
+        if not UserInfo.objects.filter(
+                username=attrs
+        ).exists():
+            raise serializers.ValidationError(detail="登录失败，用户不存在！", code="auth")
+        return attrs
+
+
+class UserTokenSerializer(serializers.ModelSerializer, ABC):
+    class Meta:
+        model = UserToken
+        fields = "__all__"
+
+    def make_token(self, username):
+        """
+        :param username:
+        :return:
+        """
+        md5 = hashlib.md5(
+            "{0}{1}{2}".format(username, time.time(), SECRET_KEY).encode("utf8")
+        )
+        return md5.hexdigest()
+
+    def validate(self, attrs):
+        attrs['token'] = self.make_token(username=attrs['username'])
+        attrs['expiration_time'] = timezone.now() + timezone.timedelta(minutes=+60)
+        attrs['update_date'] = datetime.datetime.now()
+        return attrs
+
+    def create(self, validated_data):
+        UserToken.objects.filter()
+        obj = UserToken.objects.create(**validated_data)
+        return obj
+
+
+__all__ = [
+    "SignInSerializer"
+]
---
+++ apps/account/views.py
@@ -0,0 +1,157 @@
+from django.shortcuts import render
+# Create your views here.
+from rest_framework.views import APIView
+from utils.response import APIResponse
+from apps.account.serializers import *
+from django.utils import timezone
+import datetime
+from apps.account.models import UserInfo
+
+
+class UserManager(APIView):
+
+    def get(self, requests, **kwargs):
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def put(self, requests, **kwargs):
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def delete(self, requests, **kwargs):
+        """
+        :param requests:
+        :param kwargs:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def post(self, requests):
+        """
+        :param requests:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+
+class Login(APIView):
+    def post(self, requests):
+        """
+        :param requests:
+        :return:
+        """
+        data = SignInSerializer(data=requests.data)
+        if not data.is_valid():
+            return APIResponse(
+                data=[],
+                status=200,
+                errcode="00001",
+                errmsg="登录失败,用户或账号密码错误！"
+            )
+
+        # 保存(存在就更新不存在就创建，并设置过期时间为60分钟)
+        expiration_time = timezone.now() + timezone.timedelta(minutes=+60)
+        # token = make_token(username=data.data['username'])
+        try:
+            user = UserInfo.objects.get(username=data.data['username'])
+            other = {
+                "username": UserInfo.objects.get(username=data.data['username']),
+                "defaults": {
+                    "token": token,
+                    "expiration_time": expiration_time,
+                    "update_date": datetime.datetime.now(),
+                }
+            }
+            UserToken.objects.update_or_create(**other)
+            return APIResponse(
+                code="00000",
+                msg="登录成功!",
+                token=token
+            )
+        except Exception as e:
+            return APIResponse(
+                code="00001",
+                msg="登录失败，用户token更新失败，{0}".format(e)
+            )
+
+    def get(self, requests):
+        """
+        :param requests:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="ddddddd", status=200)
+
+
+class Logout(APIView):
+    def post(self, requests):
+        """
+        :param requests:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="ddddddd", status=200)
+
+    def get(self, requests):
+        """
+        :param requests:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="ddddddd", status=200)
+
+
+class RoleManager(APIView):
+    def get(self, requests, **kwargs):
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def put(self, requests, **kwargs):
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def delete(self, requests, **kwargs):
+        """
+        :param requests:
+        :param kwargs:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def post(self, requests):
+        """
+        :param requests:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+
+class PermissionManager(APIView):
+    def get(self, requests, **kwargs):
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def put(self, requests, **kwargs):
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def delete(self, requests, **kwargs):
+        """
+        :param requests:
+        :param kwargs:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
+
+    def post(self, requests):
+        """
+        :param requests:
+        :return:
+        """
+        print(requests.data)
+        return APIResponse(data="dddd", status=200)
