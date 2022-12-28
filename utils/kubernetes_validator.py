d5e6e1489849b4bfb298d1a53ccaf3d284e1deeb
git <smxblysq@qq.com>
2022-12-28 17:43:40 +0800

    push kubernetes template

--- utils/kubernetes_validator.py
+++ utils/kubernetes_validator.py
@@ -1,8 +1,58 @@
 deployment = {
     'apiVersion': {'type': 'string', 'allowed': ['apps/v1'], 'required': True},
     'kind': {'type':'string', 'allowed': ['Deployment'], 'required': True},
-    'metadata': {'type': 'object'},
-    'spec': {'type': 'string', 'allowed': ['']},
+    'metadata': {'type': 'dict',
+        'schema': {
+            'labels': {'type': 'dict','required': False },
+            'name': {'type': 'string', 'required': True },
+            'namespace': {'type':'string','required': True }
+        },
+    },
+    'spec': {'type': 'dict', 
+    'schema':{
+        'replicas': {'type': 'integer','required': True },
+        'selector': {'type': 'dict','required': True ,'schema': {
+            'matchLabels': {'type': 'dict', 'required': True}
+            },
+        'template': {
+            'type': 'dict','required': True, 'schema': {
+                'metadata': {'type': 'dict','required': True},
+                'spec': {
+                    'type': 'dict','required': True,'schema': {
+                        'containers': {
+                            'type': 'list',
+                            'items': [{
+                                'type': 'dict','schema': {
+                                    'name': {'type': 'string','required': True},
+                                    'image': {'type': 'string','required': True},
+                                    'imagePullPolicy': {'type': 'string','required': True},
+                                    'ports': {'type': 'list','required': False, 'items': [{
+                                        'type': 'dict','required': True, 'schema': {
+                                           'protocol':{'type': 'string','required': True},
+                                           'containerPort': {'type': 'integer','required': True},
+                                        }
+                                    }]},
+                                    'resources': {'type':'dict','required': 'FaLse','schema': {
+                                        'limits': {
+                                            'type': 'dict','required': True,'schema':{
+                                                    'type': 'string','required': True,
+                                                    'cpu':{'type': 'integer','required': True},
+                                                    'memory':{'type': 'integer','required': True}
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }]
+                        }
+                    }
+                }
+            }
+        },
+        'strategy': {'type': 'dict','required': False },
+        }
+        },
+    }
 }
 
 service={
