7e13a439c3807882776fb0934cc740a513d1232e
git <smxblysq@qq.com>
2023-02-12 15:51:35 +0800

    fix bugs

--- utils/kubernetes_class.py
+++ utils/kubernetes_class.py
@@ -122,6 +122,7 @@ class KubernetesClass:
         """
         namespaces = self.list_namespace(**kwargs)
         for namespace in namespaces.items:
+            print(namespace)
             if namespace.name.startswith('u_') or namespace.name.startswith('p_'):
                 continue
             KubernetesNameSpace.objects.update_or_create(
